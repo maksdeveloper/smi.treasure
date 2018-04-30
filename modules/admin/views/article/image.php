f35722d0a05dcc85e2578bbc12725d3a67e5d498
smi <smi@codevery.com>
2018-04-30 20:52:44 +0300

    blog start

---
+++ modules/admin/views/article/image.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SMI
+ * Date: 4/27/2018
+ * Time: 4:17 PM
+ */
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Article */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="article-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'image')->fileInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Submit', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
