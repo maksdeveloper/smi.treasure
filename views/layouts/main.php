043dafd9d784c88427c3466b65407c7dd0edb291
smi <smi@codevery.com>
2018-05-24 17:22:52 +0300

    small fixes footer

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -48,7 +48,7 @@ PublicAsset::register($this);
                     </li>
                 </ul>
                 <div class="i_con">
-                    <ul class="nav navbar-nav text-uppercase">
+                    <ul class="nav navbar-nav text-uppercase d-flex">
                         <?php if(Yii::$app->user->isGuest): ?>
                         <li><a href="<?php echo Url::toRoute(['/auth/login']);?>">Login</a></li>
                         <li><a href="<?php echo Url::toRoute(['/auth/signup']);?>">Register</a></li>
@@ -77,134 +77,9 @@ PublicAsset::register($this);
 
 <?php echo $content; ?>
 
+<?php echo $this->render('/partials/footer'); ?>
 
 
-<footer class="footer-widget-section">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4">
-                <aside class="footer-widget">
-                    <div class="about-img"><img src="/uploads/images/logo2.png" alt=""></div>
-                    <div class="about-content">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-                        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed voluptua. At vero eos et
-                        accusam et justo duo dlores et ea rebum magna text ar koto din.
-                    </div>
-                    <div class="address">
-                        <h4 class="text-uppercase">contact Info</h4>
-
-                        <p> 14529/12 NK Streets, DC, KZ</p>
-
-                        <p> Phone: +123 456 78900</p>
-
-                        <p>mytreasure.com</p>
-                    </div>
-                </aside>
-            </div>
-
-            <div class="col-md-4">
-                <aside class="footer-widget">
-                    <h3 class="widget-title text-uppercase">Testimonials</h3>
-
-                    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                        <!--Indicator-->
-                        <ol class="carousel-indicators">
-                            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                            <li data-target="#myCarousel" data-slide-to="1"></li>
-                            <li data-target="#myCarousel" data-slide-to="2"></li>
-                        </ol>
-                        <div class="carousel-inner" role="listbox">
-                            <div class="item active">
-                                <div class="single-review">
-                                    <div class="review-text">
-                                        <p>Lorem ipsum dolor sit amet, conssadipscing elitr, sed diam nonumy eirmod
-                                            tempvidunt ut labore et dolore magna aliquyam erat,sed diam voluptua. At
-                                            vero eos et accusam justo duo dolores et ea rebum.gubergren no sea takimata
-                                            magna aliquyam eratma</p>
-                                    </div>
-                                    <div class="author-id">
-                                        <img src="/uploads/images/author.png" alt="">
-
-                                        <div class="author-text">
-                                            <h4>Sophia</h4>
-
-                                            <h4>CEO, ReadyTheme</h4>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="item">
-                                <div class="single-review">
-                                    <div class="review-text">
-                                        <p>Lorem ipsum dolor sit amet, conssadipscing elitr, sed diam nonumy eirmod
-                                            tempvidunt ut labore et dolore magna aliquyam erat,sed diam voluptua. At
-                                            vero eos et accusam justo duo dolores et ea rebum.gubergren no sea takimata
-                                            magna aliquyam eratma</p>
-                                    </div>
-                                    <div class="author-id">
-                                        <img src="/uploads/images/author.png" alt="">
-
-                                        <div class="author-text">
-                                            <h4>Sophia</h4>
-
-                                            <h4>CEO, ReadyTheme</h4>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="item">
-                                <div class="single-review">
-                                    <div class="review-text">
-                                        <p>Lorem ipsum dolor sit amet, conssadipscing elitr, sed diam nonumy eirmod
-                                            tempvidunt ut labore et dolore magna aliquyam erat,sed diam voluptua. At
-                                            vero eos et accusam justo duo dolores et ea rebum.gubergren no sea takimata
-                                            magna aliquyam eratma</p>
-                                    </div>
-                                    <div class="author-id">
-                                        <img src="/uploads/images/author.png" alt="">
-
-                                        <div class="author-text">
-                                            <h4>Sophia</h4>
-
-                                            <h4>CEO, ReadyTheme</h4>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </aside>
-            </div>
-            <div class="col-md-4">
-                <aside class="footer-widget">
-                    <h3 class="widget-title text-uppercase">Custom Category Post</h3>
-
-
-                    <div class="custom-post">
-                        <div>
-                            <a href="#"><img src="/uploads/images/footer-img.png" alt=""></a>
-                        </div>
-                        <div>
-                            <a href="#" class="text-uppercase">Home is peaceful Place</a>
-                            <span class="p-date">February 15, 2016</span>
-                        </div>
-                    </div>
-                </aside>
-            </div>
-        </div>
-    </div>
-    <div class="footer-copy">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="text-center">&copy; 2015 <a href="#">Treasure PRO, </a> Built with <i
-                                class="fa fa-heart"></i> by <a href="#">Rahim</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</footer>
 <?php $this->endBody() ?>
 </body>
 </html>
---
+++ views/partials/footer.php
@@ -0,0 +1,42 @@
+<footer class="footer-widget-section">
+    <div class="container">
+        <div class="footer-first row d-flex ">
+            <div class="col-md-4">
+                <aside class="footer-widget">
+                    <div class="about-img"><img src="/uploads/images/logo2.png" alt=""></div>
+
+
+                </aside>
+            </div>
+
+            <div class="col-md-4">
+                <div class="about-content">This is a simple blog on Yii2
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="address d-flex">
+                    <div class="contact">
+                        <h4 class="text-uppercase">contact Info</h4>
+                        <p> Zaporozhye, Ukraine </p>
+                    </div>
+                    <div class="social d-flex">
+
+                    <p><a href="https://www.facebook.com/maksym.smi"><i class="fa fa-facebook"></a></p></i>
+                    <p><a href="https://www.linkedin.com/in/maksym-snigach-b16593151/"><i class="fa fa-linkedin"></a></p></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copy">
+        <div class="container">
+            <div class="footer-second row">
+                <div class="col-md-12">
+                    <div class="text-center">&copy; <?php echo date('Y');?> <a href="#">Treasure Blog, </a> Built with <i
+                            class="fa fa-heart"></i> by <a href="#">SmyCodevery</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
